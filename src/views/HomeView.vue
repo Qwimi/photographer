8072f36fa92f192dbdfdcff5ec8eca9b3e0e3825
Qwimi <133797329+Qwimi@users.noreply.github.com>
2024-03-09 14:44:48 +0400

    init commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    тут будет обо мне
+    <!-- <TheWelcome /> -->
+  </main>
+</template>
