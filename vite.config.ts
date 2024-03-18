ea7047d9a15d057aed710eaab130bf3cd8e6c58e
Qwimi <133797329+Qwimi@users.noreply.github.com>
2024-03-19 02:05:51 +0400

    assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import copy from 'rollup-plugin-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
