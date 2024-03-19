80cdd98a9712259d01b9952b5a3d1c38cc8b4d7f
Qwimi <133797329+Qwimi@users.noreply.github.com>
2024-03-19 13:44:54 +0400

    aaa

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/photographer',
+  // base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
